e94ab5904c57c49baa622d8cfb64adf652aea69d
Tom[ Hsieh, Yu-Hsuan ] <tom.hsieh@linecorp.com>
2023-03-15 17:51:05 +0800

    main logic

--- src/main/kotlin/budget/BudgetService.kt
+++ src/main/kotlin/budget/BudgetService.kt
@@ -2,13 +2,44 @@ package budget
 
 import java.time.LocalDate
 import java.time.YearMonth
+import java.util.DoubleSummaryStatistics
 
 class BudgetService(
     private val budgetRepo: BudgetRepo
 ){
 
-    fun query(startDate: LocalDate, endDate: LocalDate){
+    fun query(startDate: LocalDate, endDate: LocalDate): Double {
+        val yearMonthBudgetMap: Map<YearMonth, Budget> = getRange(
+            start = YearMonth.of(startDate.year, startDate.month),
+            end = YearMonth.of(endDate.year, endDate.month)
+        ).associateBy { YearMonth.of(it.getYearMonthDate().year, it.getYearMonthDate().monthValue) }
 
+        var yearMonth = YearMonth.of(startDate.year, startDate.month)
+        val endYearMonth = YearMonth.of(endDate.year, endDate.month)
+        val dayAmountMap = mutableMapOf<YearMonth, Double>()
+        while(yearMonth <= endYearMonth) {
+            val monthBudget = yearMonthBudgetMap[yearMonth] ?: Budget(yearMonth.toMyString())
+            dayAmountMap[yearMonth] = getDayBudget(monthBudget)
+            yearMonth = yearMonth.plusMonths(1)
+        }
+        val map = mutableMapOf<YearMonth, Int>()
+        var date = startDate
+        while(date <= endDate) {
+            val keyMonth = YearMonth.of(date.year, date.month)
+            if (map.containsKey(keyMonth)) {
+                map[keyMonth] = map[keyMonth]!!.plus(1)
+            } else {
+                map[keyMonth] = 1
+            }
+            date = date.plusDays(1)
+        }
+
+        var amount = 0.00
+        map.forEach{ (yearMonth, day) ->
+            amount += day* (dayAmountMap[yearMonth] ?: 0.00)
+        }
+
+        return amount
     }
 
     fun getRange(start: YearMonth, end: YearMonth): List<Budget> {
@@ -17,7 +48,15 @@ class BudgetService(
 
         return budgetList.filter {
             val yearMonth = it.getYearMonth()
-            yearMonth.isAfter(start) && yearMonth.isBefore(end)
+            val compareToEnd = yearMonth.compareTo(end)
+            val compareToStart = yearMonth.compareTo(start)
+            (compareToStart >= 1 || compareToStart == 0) && (compareToEnd <= -1 || compareToEnd == 0)
         }
     }
+
+    fun getDayBudget(budget: Budget): Double {
+      return budget.amount.toDouble()/YearMonth.of(budget.getYearMonth().year, budget.getYearMonth().monthValue).lengthOfMonth()
+    }
+
+    fun YearMonth.toMyString():String = this.toString().replace("-", "")
 }
