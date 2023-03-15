f678c1a2f63bdd6a7fc2c3e52451ebe63b7567ae
Tom[ Hsieh, Yu-Hsuan ] <tom.hsieh@linecorp.com>
2023-03-15 16:57:19 +0800

    fixed to localDate

--- src/main/kotlin/budget/Budget.kt
+++ src/main/kotlin/budget/Budget.kt
@@ -1,15 +1,15 @@
 package budget
 
-import java.util.*
+import java.time.LocalDate
 
 data class Budget(
     val yearMonth: String = "",
     val amount: Int = 0
 ){
 
-    fun getYearMonthDate() : Date {
+    fun getYearMonthDate() : LocalDate {
         println(yearMonth.substring(0,4))
         println(yearMonth.substring(5,6))
-        return Date(yearMonth.substring(0,3).toInt(), yearMonth.substring(3,5).toInt(), 1)
+        return LocalDate.of(yearMonth.substring(0,3).toInt(), yearMonth.substring(3,5).toInt(), 1)!!
     }
 }
