445b09aa298b69727c193f872649db9ddfde5694
Tom[ Hsieh, Yu-Hsuan ] <tom.hsieh@linecorp.com>
2023-03-15 16:58:01 +0800

    getRange

--- src/main/kotlin/budget/BudgetService.kt
+++ src/main/kotlin/budget/BudgetService.kt
@@ -9,4 +9,16 @@ class BudgetService(
     fun query(startDate: LocalDate, endDate: LocalDate){
 
     }
+
+    fun getRange(start: LocalDate, end: LocalDate): List<Budget> {
+        if (end.isBefore(start)) return emptyList()
+        val budgetList: List<Budget> = budgetRepo.getAll()
+
+        return budgetList.filter {
+            if (it.yearMonth.length != 6) return@filter false
+            val year = it.yearMonth.substring(0, 4).toInt()
+            val month = it.yearMonth.substring(4, it.yearMonth.length).trimStart('0').toInt()
+            year >= start.year && year <= end.year && month >= start.month.value && month <= end.month.value
+        }
+    }
 }
